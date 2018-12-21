2539c8b483af3f2d63c244ca5f27361e9cde6ac5
1837264450 <1837264450@qq.com>
2018-12-21 15:31:07 +0800

    抽离postcss配置

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      features: {
+        'nesting-rules': true,
+        'color-mod-function': { unresolved: 'warn' }
+      },
+      stage: 0,
+    },
+    'postcss-flexbugs-fixes': {},
+
+  }
+}
